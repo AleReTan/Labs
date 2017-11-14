2cf0016fd05ceb49a24c710cd7b839839a33c6f5
AleReTan <reshetnikov.alex93@gmail.com>
2017-11-14 21:02:35 +0300

    Add some fields

--- Lab1/src/main/java/ru/vsu/Person.java
+++ Lab1/src/main/java/ru/vsu/Person.java
@@ -1,4 +1,51 @@
 package ru.vsu;
 
 public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int age;
+    private LocalDate birthday;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return Person.class.getSimpleName() +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
