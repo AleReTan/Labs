2832b2cab358dbf8285f6987e634554077bd334d
AleReTan <reshetnikov.alex93@gmail.com>
2017-12-07 16:24:37 +0300

    minor changes

---
+++ Lab1/src/main/java/ru/vsu/util/Configurator.java
@@ -0,0 +1,39 @@
+package ru.vsu.util;
+
+import ru.vsu.sorter.PersonSorter;
+import ru.vsu.sorter.sorterImpl.BubblePersonSorter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configurator {
+    private static Configurator instance;
+
+    private Configurator() {
+    }
+
+    public PersonSorter getSorter() {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "configuration.properties";
+
+        Properties appProps = new Properties();
+        try {
+            appProps.load(new FileInputStream(appConfigPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String appSorter = appProps.getProperty("sorter");
+        switch (appSorter){
+            case "bubble":  return new BubblePersonSorter();
+            default:return new BubblePersonSorter();
+        }
+    }
+
+    public static Configurator getInstance() {
+        if (instance == null) {
+            instance = new Configurator();
+        }
+        return instance;
+    }
+}
